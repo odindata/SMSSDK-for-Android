adf6d4f89c61d7d1fe47a7a51fc33de8fdbae6f3
hechao <he_chao_1198@163.com>
2020-06-04 15:45:45 +0800

    1、去掉日志打印；2、增加gradle.properties可调试的语句

--- app/src/main/java/com/odin/sms/demo/OdinApplication.java
+++ app/src/main/java/com/odin/sms/demo/OdinApplication.java
@@ -12,6 +12,5 @@ public class OdinApplication extends Application {
 
         //初始化OdinSMSSDK
         OdinSMS.init(this, "985459861c2c4e7b8f4f2c7245e56448", "b250f2ba017048e88275154a62ed1bce");
-        OdinSMS.setLogEnabled(true);
     }
 }
